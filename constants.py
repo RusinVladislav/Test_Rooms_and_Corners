10ad8c670106cbd3478f9959223f69706722c268
Vladislav <xxx.swat.ru@gmail.com>
2023-08-27 14:06:20 +0300

    release: reads json file passed as parameter as a pandas dataframe and draws plot for comparing different columns

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-FILE_URL = 'https://ai-process-sandy.s3.eu-west-12.amazonaws.com/purge/deviation.json'
+FILE_URL = 'https://ai-process-sandy.s3.eu-west-1.amazonaws.com/purge/deviation.json'
 FILE_NAME = 'data_example.json'
 FILE_NAME_ONLINE = 'data_online.json'
